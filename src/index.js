26f419b87b4e88076b69a9dce49c1c9f6dd8cda9
shuibuzhuo <2301140231@qq.com>
2023-11-24 14:11:32 +0800

    feat: loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import './index.css';
+import value from './test.shuibuzhuo';
 
 console.log('hello webpack!');
+console.log(value);
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.shuibuzhuo$/,
+        use: [path.resolve(__dirname, './loader/shuibuzhuo-loader.js')],
+      },
     ],
   },
 };
